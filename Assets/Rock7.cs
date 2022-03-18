0ca488313e00388421f7e8de62b94aa92cce0eb6
Laura17lara <99354167+Laura17lara@users.noreply.github.com>
2022-03-18 17:40:45 -0500

    Rock6

---
+++ Assets/Rock7.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using  UnityEngine.SceneManagement;
+
+public class Rock7 : MonoBehaviour {
+    
+    void OnCollisionEnter(Collision collision){
+        if (collision.gameObject.tag=="Carro") {
+    
+            Debug.Log
+        
+    }
+}
