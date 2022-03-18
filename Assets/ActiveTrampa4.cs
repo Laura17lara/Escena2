29b0320074cc656e4969fcc0e5be87f375d8ddbf
Laura17lara <99354167+Laura17lara@users.noreply.github.com>
2022-03-18 14:12:54 -0500

    Trampa4

---
+++ Assets/ActiveTrampa4.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActiveTrampa4 : MonoBehaviour {
+    public GameObject activarTrampa;
+    public Rigidbody rock;
+    
+    void OnTriggerEnter(Collider other){
+        if (other.gameObject.tag == "Carro") {
+            rock.GetComponent<Rigidbody> ().useGravity = true;
+        }  
+    }
+}
